26dcd5ace03ffda4074aac47371a6799ea195888
thanhpp <thanhphanphu18@gmail.com>
2022-10-18 11:36:07 +0700

    test: 🚧 update
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- github-analyze/src/main.rs
+++ github-analyze/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let client = Client::new();
+
+    let uri = "http://httpbin.org/ip".parse()?;
+
+    let resp = client.get(uri).await?;
+
+    println!("Response {}", resp.status());
+
+    Ok(())
 }

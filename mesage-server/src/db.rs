24e42bf31301ddff8b8d40c434096f4bc0d64165
thanhpp <thanhphanphu18@gmail.com>
2023-03-17 00:53:36 +0700

    feat: ✨ message-server: get messages from db
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- mesage-server/src/db.rs
+++ mesage-server/src/db.rs
@@ -1,9 +1,13 @@
-use diesel::prelude::*;
+use diesel::{
+    prelude::*,
+    sql_query,
+    sql_types::{BigInt, Text},
+};
 use serde::Serialize;
 
 use crate::schema::messages;
 
-#[derive(Queryable, Insertable, Clone, Serialize)]
+#[derive(Queryable, Insertable, Clone, Serialize, QueryableByName, Debug)]
 #[diesel(table_name = messages)]
 pub struct Message {
     pub timestamp: i64,
@@ -21,16 +25,35 @@ pub fn create_message(conn: &mut PgConnection, msg: &Message) -> Result<(), dies
     }
 }
 
+pub fn get_message_by_user(
+    conn: &mut PgConnection,
+    usrname: String,
+    from: i64,
+    to: i64,
+) -> Result<Vec<Message>, diesel::result::Error> {
+    let q = sql_query("SELECT * FROM messages WHERE username = $1 AND timestamp BETWEEN $2 AND $3");
+    let msgs = match q
+        .bind::<Text, _>(usrname)
+        .bind::<BigInt, _>(from) // does not implement for i64, kinda weird
+        .bind::<BigInt, _>(to) // does not implement for i64, kinda weird
+        .get_results(conn)
+    {
+        Ok(x) => x,
+        Err(e) => return Err(e),
+    };
+
+    Ok(msgs)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn establish_connection() -> Result<PgConnection, ConnectionError> {
+        PgConnection::establish("postgres://username:password@localhost:5432/demo")
+    }
     #[test]
     fn test_create_message() {
-        pub fn establish_connection() -> Result<PgConnection, ConnectionError> {
-            PgConnection::establish("postgres://username:password@localhost:5432/demo")
-        }
-
         let conn = &mut establish_connection().expect("connect db error");
         create_message(
             conn,
@@ -41,5 +64,11 @@ mod tests {
             },
         )
         .unwrap();
+    }
+
+    #[test]
+    fn test_get_message_by_user() {
+        let conn = &mut establish_connection().expect("connect db error");
+        get_message_by_user(conn, "thanhpp".into(), 0, 1).unwrap();
     }
 }
--- mesage-server/src/main.rs
+++ mesage-server/src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use actix_web::{get, web, App, HttpResponse, HttpServer};
 use chrono::Utc;
 use deadpool_diesel::postgres::{Manager, Pool};
+use serde::Deserialize;
 
 mod db;
 mod dto;
@@ -22,6 +23,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(pool.clone()))
             .service(get)
             .service(web::resource("/").route(web::post().to(post_message)))
+            .service(get_messages)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -48,11 +50,47 @@ async fn post_message(
         .interact(move |conn| db::create_message(conn, &insert_db_msg))
         .await
     {
-        Ok(_) => Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .json(db_msg)),
+        Ok(_) => {
+            println!("{:?}", &db_msg);
+            Ok(HttpResponse::Ok()
+                .content_type("application/json")
+                .json(db_msg))
+        }
         Err(e) => Err(e.into()),
     }
+}
+
+#[derive(Deserialize)]
+struct GetMessagesQuery {
+    username: String,
+    from: i64,
+    to: i64,
+}
+
+#[get("/messages")]
+async fn get_messages(
+    pool: web::Data<Pool>,
+    query: web::Query<GetMessagesQuery>,
+) -> Result<HttpResponse, Box<dyn Error>> {
+    println!(
+        "get messages query {} {} {}",
+        &query.username, &query.from, &query.to
+    );
+
+    let conn = match pool.get().await {
+        Ok(c) => c,
+        Err(e) => return Err(e.into()),
+    };
+
+    let msgs = conn
+        .interact(move |conn| {
+            db::get_message_by_user(conn, query.username.clone(), query.from, query.to)
+        })
+        .await??;
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .json(msgs))
 }
 
 #[get("/")]

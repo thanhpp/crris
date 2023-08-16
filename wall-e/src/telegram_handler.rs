5b97f334336061c5dc263673f5a3980c30546cbd
thanhpp <thanhphanphu18@gmail.com>
2023-08-16 15:52:49 +0700

    feat: 🚧 wall-e: parse string
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- wall-e/src/telegram_handler.rs
+++ wall-e/src/telegram_handler.rs
@@ -13,16 +13,8 @@ enum Command {
     Help,
     #[command(description = "Show balance")]
     ShowBalance,
-    #[command(description = "Add balance", parse_with = "split")]
-    AddBalance {
-        user: String,
-        op_1: String,
-        amount_1: String,
-        unit_1: String,
-        op_2: String,
-        amount_2: String,
-        unit_2: String,
-    },
+    #[command(description = "Add balance")]
+    AddBalance(String),
 }
 
 pub async fn start(token: &str) {
@@ -42,25 +34,9 @@ async fn handle_message(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<
                 .await?
         }
         Command::ShowBalance => bot.send_message(msg.chat.id, "show_balanace").await?,
-        Command::AddBalance {
-            user,
-            op_1,
-            amount_1,
-            unit_1,
-            op_2,
-            amount_2,
-            unit_2,
-        } => {
+        Command::AddBalance(s) => {
             let date = get_date();
-            let b_op = match BalanceOperation::new(&Command::AddBalance {
-                user,
-                op_1,
-                amount_1,
-                unit_1,
-                op_2,
-                amount_2,
-                unit_2,
-            }) {
+            let b_op = match BalanceOperation::new(&s) {
                 Err(e) => format!("{:#?}", e),
                 Ok(b_op) => format!("{:#?}", b_op),
             };
@@ -91,60 +67,64 @@ struct BalanceOperation {
 }
 
 impl BalanceOperation {
-    fn new(c: &Command) -> anyhow::Result<BalanceOperation> {
+    fn new(s: &str) -> anyhow::Result<BalanceOperation> {
         let valid_users = vec![String::from("tpp"), String::from("pch")];
         let valid_ops = vec![String::from("+"), String::from("-")];
         let valid_units = vec![String::from("vnd"), String::from("pl")];
         let mut b_op = BalanceOperation::default();
 
-        match c {
-            Command::AddBalance {
-                user,
-                op_1,
-                amount_1,
-                unit_1,
-                op_2,
-                amount_2,
-                unit_2,
-            } => {
-                let user = user.to_lowercase();
-                if !valid_users.contains(&user) {
-                    return Err(anyhow::format_err!("invalid user: [{}]", user));
-                }
-                b_op.user = user;
+        let data = s.split(' ').collect::<Vec<&str>>();
+        if data.len() != 4 && data.len() != 7 {
+            return Err(anyhow::format_err!("invalid length {}", data.len()));
+        }
 
-                if !valid_ops.contains(op_1) {
-                    return Err(anyhow::format_err!("invalid op 1: [{}]", op_1));
-                }
-                if op_1.as_str() == "+" {
-                    b_op.sign_positive_1 = true;
-                }
+        let user = data[0].to_lowercase();
+        let op_1 = data[1].to_lowercase();
+        let amount_1 = data[2].to_lowercase();
+        let unit_1 = data[3].to_lowercase();
 
-                b_op.amount_1 = Self::parse_amount(amount_1)?;
+        if !valid_users.contains(&user) {
+            return Err(anyhow::format_err!("invalid user: [{}]", user));
+        }
+        b_op.user = user;
 
-                let unit_1 = unit_1.to_lowercase();
-                if !valid_units.contains(&unit_1) {
-                    return Err(anyhow::format_err!("invalid unit 1: [{}]", unit_1));
-                }
-                b_op.unit_1 = unit_1;
+        if !valid_ops.contains(&op_1) {
+            return Err(anyhow::format_err!("invalid op 1: [{}]", op_1));
+        }
+        if op_1.as_str() == "+" {
+            b_op.sign_positive_1 = true;
+        }
 
-                if !valid_ops.contains(op_2) {
-                    return Err(anyhow::format_err!("invalid op 2: [{}]", op_2));
-                }
-                if op_2.as_str() == "+" {
-                    b_op.sign_positive_2 = true;
-                }
+        b_op.amount_1 = Self::parse_amount(&amount_1)?;
 
-                b_op.amount_2 = Self::parse_amount(amount_2)?;
+        let unit_1 = unit_1.to_lowercase();
+        if !valid_units.contains(&unit_1) {
+            return Err(anyhow::format_err!("invalid unit 1: [{}]", unit_1));
+        }
+        b_op.unit_1 = unit_1;
 
-                let unit_2 = unit_2.to_lowercase();
-                if !valid_units.contains(&unit_2) {
-                    return Err(anyhow::format_err!("invalid unit 2: [{}]", unit_2));
-                }
-                b_op.unit_2 = unit_2;
-            }
-            _ => return Err(anyhow::format_err!("not add balance command")),
-        };
+        if data.len() != 7 {
+            return Ok(b_op);
+        }
+
+        let op_2 = data[4].to_lowercase();
+        let amount_2 = data[5].to_lowercase();
+        let unit_2 = data[6].to_lowercase();
+
+        if !valid_ops.contains(&op_2) {
+            return Err(anyhow::format_err!("invalid op 2: [{}]", op_2));
+        }
+        if op_2.as_str() == "+" {
+            b_op.sign_positive_2 = true;
+        }
+
+        b_op.amount_2 = Self::parse_amount(&amount_2)?;
+
+        let unit_2 = unit_2.to_lowercase();
+        if !valid_units.contains(&unit_2) {
+            return Err(anyhow::format_err!("invalid unit 2: [{}]", unit_2));
+        }
+        b_op.unit_2 = unit_2;
 
         Ok(b_op)
     }
